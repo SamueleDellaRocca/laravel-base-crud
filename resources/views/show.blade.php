e3ccb12c18f524ba057ee3e62007b4681d502b30
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-09 18:40:51 +0200

    show.blade, con link alla home

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,13 +8,16 @@
     <div class="container">
         <div class="row">
             <div class="bg-grigio card text-center col-5">
-                <h1>{{ $comic->title }}</a>  </h1>
+                <h1>{{ $comic->title }}  </h1>
                 <div class="img">
                     <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
                 </div>
                 <p>{{ $comic->description }}</p>
                 <div>Prezzo: {{ $comic->price }}</div>
                 <div class="text-end">Data di Uscita: {{ $comic->sale_date }}</div>
+            </div>
+
+            <h2><a href="{{ route('comic.index') }}">HOMEPAGE</a></h2>
         </div>
     </div>
 @endsection
--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('pageTitle')</title>
 
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 <body>
