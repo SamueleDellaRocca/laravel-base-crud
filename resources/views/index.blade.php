a4b7d1fe1d0edc0545cc6067f8a5375cada9f834
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-09 17:43:31 +0200

    carte fumetti

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,9 +8,19 @@
 @section('pageMain')
 <main>
     <div class="container">
-        <div class="row">
+        <div class="row g-3 gx-3 gap-3 justify-content-center">
             @foreach ($comics as $comic)
-            <h1> {{ $comic->title }} </h1>
+            <div class="bg-grigio card text-center col-5">
+                <h1> {{ $comic->title }} </h1>
+                <div class="img">
+                    <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                </div>
+                <p>{{ $comic->description }}</p>
+                <div>{{ $comic->price }}</div>
+                <div class="text-end">{{ $comic->sale_date }}</div>
+
+            </div>
+           
             @endforeach
         </div>
         {{ $comics->links() }}
