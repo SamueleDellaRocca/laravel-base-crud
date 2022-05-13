4680db0dff0ec11e6856e9aeebf1a03f8e56f9c4
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-13 18:18:44 +0200

    old value

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -22,36 +22,36 @@
 
             <div class="form-group mb-5">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" name="title" id="title">
+                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
             </div>
 
             <div class="form-group mb-5">
                 <label for="thumb">URL Immagine</label>
-                <input type="text" class="form-control" name="thumb" id="thumb">
+                <input type="text" class="form-control" name="thumb" id="thumb" value="{{ old('thumb') }}">
             </div>
 
             <div class="form-group mb-5">
                 <label for="description">Description</label>
-                <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                <textarea class="form-control" id="description" rows="3" name="description">{{ old('description') }}</textarea>
             </div>
 
             <div class="form-group mb-5">
                 <label for="series">Series</label>
-                <input type="text" class="form-control" name="series" id="series">
+                <input type="text" class="form-control" name="series" id="series" value="{{ old('series') }}">
             </div>
 
             <div class="form-group mb-5">
                 <label for="price">Price</label>
-                <input type="number" class="form-control" name="price" id="price">
+                <input type="number" class="form-control" name="price" id="price" value="{{ old('series') }}">
             </div>
 
             <div class="form-group mb-5">
                 <label for="sale_date">Sale_Date</label>
-                <input type="date" class="form-control" name="sale_date" id="sale_date">
+                <input type="date" class="form-control" name="sale_date" id="sale_date" value="{{ old('sale_date') }}">
             </div>
 
             <div class="input-group mb-5">
-                <select class="custom-select p-2" id="type" name="type">
+                <select class="custom-select p-2" id="type" name="type" value="{{ old('type') }}">
                     <option selected value="comic book">Comic Book</option>
                     <option value="graphic novel">Graphic Novel</option>
                 </select>
