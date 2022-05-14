478b17e9e8689a885409ead9499674fd42adf598
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-14 17:11:55 +0200

    url previous in show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,14 +8,15 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="bg-grigio card text-center col-5">
-                <h1>{{ $comic->title }} </h1>
-                <div class="img">
-                    <img class="w-50" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-                </div>
-                <p>{{ $comic->description }}</p>
-                <div>Prezzo: {{ $comic->price }}</div>
-                <div class="text-end">Data di Uscita: {{ $comic->sale_date }}</div>
-
+                <a class="link_mine" href="{{ url()->previous() }}">
+                    <h1>{{ $comic->title }} </h1>
+                    <div class="img">
+                        <img class="w-50" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                    </div>
+                    <p>{{ $comic->description }}</p>
+                    <div>Prezzo: {{ $comic->price }}</div>
+                    <div class="text-end">Data di Uscita: {{ $comic->sale_date }}</div>
+                </a>
                 <div class="d-flex justify-content-center gap-2">
                     <a class="btn btn-success mb-2" href="{{ route('comic.edit', $comic->id) }}">EDIT</a>
                     <form action="{{ route('comic.destroy', $comic->id) }}" method="post">
